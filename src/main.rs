778858d0e6e9b7951fb2eb4358c82e5011296401
Dubs <julien.du.bois@starkleytech.com>
2024-12-14 00:19:52 +0400

    refactor: Refactor transport setup and add basic behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,42 @@
 use libp2p::{
     core::{
-        upgrade::{DeniedUpgrade},
+        muxing::StreamMuxerBox,
+        transport::{Boxed, Transport},
+        upgrade::{self, DeniedUpgrade},
     },
     identity,
     noise,
     SwarmBuilder,
     swarm::{
-        handler::ConnectionHandler,
-        NetworkBehaviour,
-        ConnectionHandlerEvent,
-        SubstreamProtocol,
-        SwarmEvent,
+        ConnectionEvent, ConnectionId, NetworkBehaviour, ConnectionHandler,
+        ConnectionHandlerEvent, SubstreamProtocol, SwarmEvent,
     },
     tcp,
     websocket,
-    Multiaddr,
-    PeerId,
+    webrtc,
+    Multiaddr, PeerId,
+    futures::StreamExt,
 };
 use libp2p_mplex::MplexConfig;
 use std::error::Error;
+use std::time::Duration;
 use std::task::{Context, Poll};
 use void::Void;
 
-#[derive(NetworkBehaviour)]
+#[derive(NetworkBehaviour, Default)]
 #[behaviour(connection_handler = "MyHandler", out_event = "MyBehaviourEvent")]
-pub struct MyBehaviour;
+pub struct MyBehaviour {
+}
 
 #[derive(Debug)]
 pub enum MyBehaviourEvent {}
-
-impl Default for MyBehaviour {
-    fn default() -> Self {
-        Self
-    }
-}
 
 #[derive(Default)]
 pub struct MyHandler;
 
 impl ConnectionHandler for MyHandler {
-    type FromBehaviour = Void;
-    type ToBehaviour = Void;
+    type InEvent = Void;
+    type OutEvent = Void;
     type InboundProtocol = DeniedUpgrade;
     type OutboundProtocol = DeniedUpgrade;
     type InboundOpenInfo = ();
@@ -50,7 +46,7 @@ impl ConnectionHandler for MyHandler {
         SubstreamProtocol::new(DeniedUpgrade, ())
     }
 
-    fn on_behaviour_event(&mut self, event: Self::FromBehaviour) {
+    fn on_behaviour_event(&mut self, event: Self::InEvent) {
         void::unreachable(event)
     }
 
@@ -65,7 +61,7 @@ impl ConnectionHandler for MyHandler {
         ConnectionHandlerEvent<
             Self::OutboundProtocol,
             Self::OutboundOpenInfo,
-            Self::ToBehaviour,
+            Self::OutEvent,
         >,
     > {
         Poll::Pending
@@ -73,15 +69,39 @@ impl ConnectionHandler for MyHandler {
 
     fn on_connection_event(
         &mut self,
-        _event: libp2p::swarm::handler::ConnectionEvent<
+        event: ConnectionEvent<
             Self::InboundProtocol,
             Self::InboundOpenInfo,
             Self::OutboundProtocol,
             Self::OutboundOpenInfo,
         >,
     ) {
+        match event {
+            _ => {}
+        }
     }
 }
+
+
+async fn build_transport() -> Result<Boxed<()>, Box<dyn Error>> {
+    let tcp_transport = tcp::tokio::Transport::new(tcp::Config::default());
+    let ws_transport = websocket::tokio::Transport::new(websocket::Config::default(), tcp::tokio::Transport::new(tcp::Config::default()));
+    let webrtc_transport = webrtc::tokio::Transport::new(webrtc::Config::default());
+
+    let noise_config = noise::Config::new();
+    let mplex_config = MplexConfig::new();
+
+    let transport = tcp_transport
+        .or_transport(ws_transport)
+        .or_transport(webrtc_transport)
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise_config)
+        .multiplex(mplex_config)
+        .boxed();
+
+    Ok(transport)
+}
+
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -89,16 +109,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
 
+    let transport = build_transport().await?;
+
     let mut swarm = SwarmBuilder::with_existing_identity(local_key)
         .with_tokio()
-        .with_tcp(tcp::Config::default(), noise::Config::new, || {
-            MplexConfig::new()
-        })?
-        .with_websocket(noise::Config::new, || {
-            MplexConfig::new()
-        })?
+        .with_other_transport(transport)
         .with_behaviour(|_| MyBehaviour::default())?
-        .with_swarm_config(|_| Default::default())
         .build();
 
     let addr: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse()?;
@@ -109,10 +125,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             SwarmEvent::NewListenAddr { address, .. } => {
                 println!("Listening on {:?}", address);
             }
-            SwarmEvent::IncomingConnection { .. } => {
-                println!("Incoming connection");
-            }
-            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+             SwarmEvent::ConnectionEstablished { peer_id, .. } => {
                 println!("Connection established with {:?}", peer_id);
             }
             SwarmEvent::ConnectionClosed { peer_id, .. } => {
