631f38d22dc68b7edf5efcf2d436e76fb253009f
Dubs <julien.du.bois@starkleytech.com>
2024-12-15 17:10:49 +0400

    fix: Remove explicit Transport import from libp2p core

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::hash::{Hash, Hasher, DefaultHasher};
 use futures_util::{StreamExt, SinkExt, stream::Stream};
 use libp2p::{
     core::{
-        transport::{OrTransport, Transport},
+        transport::OrTransport,
         upgrade,
     },
     gossipsub::{self},
