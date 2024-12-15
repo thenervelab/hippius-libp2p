3ce0b08a619e06918432061dd1f654c64e2342ee
Dubs <julien.du.bois@starkleytech.com>
2024-12-15 17:13:48 +0400

    fix: Correctly initialize WebRTC transport

--- src/main.rs
+++ src/main.rs
@@ -139,8 +139,7 @@ impl P2pServer {
 
         // Create and configure WebRTC transport
         let cert = Certificate::generate(&mut thread_rng())?;
-        let webrtc_transport = libp2p_webrtc::tokio::Transport::new(local_key.clone(), cert)
-            .with_config(libp2p_webrtc::WebRTCTransportConfig::new());
+        let webrtc_transport = libp2p_webrtc::tokio::Transport::new(local_key.clone(), cert);
         let webrtc_boxed = webrtc_transport
             .listen_on("/ip4/0.0.0.0/udp/0/webrtc".parse().unwrap())?
             .map(|(_, conn)| ((), StreamMuxerBox::new(conn)))
